bb7272234e53232e4e2372dcbc44b8dc35eeff0f
thakurSaad <thakursaad613@gmail.com>
2022-03-01 00:17:01 +0600

    loadData

--- js/app.js
+++ js/app.js
@@ -1,1 +1,14 @@
-console.log('connected');
+// hit api
+const loadData = () => {
+    const searchField = document.getElementById('search-field');
+    const searchText = searchField.value;
+    // clear searchField
+    searchField.value = "";
+    url = `
+        https://openapi.programming-hero.com/api/phones?search=${searchText}
+    `
+    fetch(url)
+    .then(res => res.json())
+    .then(data => console.log(data));
+    // console.log(searchText, url);
+}
