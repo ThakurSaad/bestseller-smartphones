d5eceaeaccff7ed051d69711c49b5b1c3f1de3e0
thakurSaad <thakursaad613@gmail.com>
2022-03-01 00:53:31 +0600

    search result added

--- js/app.js
+++ js/app.js
@@ -1,14 +1,33 @@
 // hit api
 const loadData = () => {
-    const searchField = document.getElementById('search-field');
-    const searchText = searchField.value;
-    // clear searchField
-    searchField.value = "";
-    url = `
+  const searchField = document.getElementById("search-field");
+  const searchText = searchField.value;
+  // clear searchField
+  searchField.value = "";
+
+  url = `
         https://openapi.programming-hero.com/api/phones?search=${searchText}
-    `
-    fetch(url)
-    .then(res => res.json())
-    .then(data => console.log(data));
-    // console.log(searchText, url);
-}
+    `;
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => displayPhones(data.data)); // get data array
+};
+
+const displayPhones = (phones) => {
+  const resultContainer = document.getElementById("result-container");
+  phones.forEach((phone) => {
+    console.log(phone);
+    const div = document.createElement("div");
+    div.innerHTML = `
+        <div onclick="loadDetails()" class="card h-100">
+            <img src="${phone.image}" class="card-img-top p-2" alt="image">
+            <div class="card-body">
+                <h5 class="card-title">${phone.brand}</h5>
+                <h3>${phone.phone_name}</h3>
+                <button class="btn btn-dark">Details</button>
+            </div>
+        </div>
+      `;
+      resultContainer.appendChild(div);
+  });
+};
